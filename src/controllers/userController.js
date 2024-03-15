4033e72aa629414477e709b7a04b7870ea760342
안창완 <waney@anchang-wan-ui-MacBookPro.local>
2024-03-15 21:14:01 +0900

    router

---
+++ src/controllers/userController.js
@@ -0,0 +1,13 @@
+export const join = (req, res) => res.send("join");
+export const remove = (req, res) => {
+  res.send("delete");
+};
+export const edit = (req, res) => res.send("edit user");
+
+export const login = (req, res) => {
+  res.send("login");
+};
+
+export const logout = (req, res) => res.send("logout");
+
+export const see = (req, res) => res.send("see");
---
+++ src/controllers/videoController.js
@@ -0,0 +1,9 @@
+export const trending = (req, res) => res.send("Home paoge videos");
+export const see = (req, res) => res.send("watch");
+export const search = (req, res) => res.send("search");
+
+export const edit = (req, res) => {
+  res.send("edit");
+};
+export const upload = (req, res) => res.send("upload");
+export const deleteVideo = (req, res) => res.send("deleteVideo");
